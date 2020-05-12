2c3fb0a52a79cc48cbe2ade1f8d64705b60fb808
aditya9970 <54310516+aditya9970@users.noreply.github.com>
2020-05-12 08:33:01 -0700

    Updates

---
+++ precache-manifest.99404218cc98cd5235904239ad5cffde.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3371793f2ec9b57ac1a34e9fe0baabd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bea490a5f90fe3efa401",
+    "url": "/static/css/main.c49ec7ba.chunk.css"
+  },
+  {
+    "revision": "1a98a69ecb58d8af4949",
+    "url": "/static/js/2.2c8de82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c8de82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea490a5f90fe3efa401",
+    "url": "/static/js/main.73958174.chunk.js"
+  },
+  {
+    "revision": "5cfe63f3d0aa65dc782b",
+    "url": "/static/js/runtime-main.dbafcd4d.js"
+  },
+  {
+    "revision": "db554c71851a0e47bde9e862d06c521e",
+    "url": "/static/media/clear-bg.db554c71.jpg"
+  },
+  {
+    "revision": "3030fd0f0b9d406781bf7b24b5e58137",
+    "url": "/static/media/cloud-bg.3030fd0f.jpg"
+  },
+  {
+    "revision": "7d8e0f977706ad1863bb2df6c95c34c8",
+    "url": "/static/media/rain-bg.7d8e0f97.jpg"
+  },
+  {
+    "revision": "be13613806036bea7f09c53168821bcf",
+    "url": "/static/media/snow-bg.be136138.jpg"
+  },
+  {
+    "revision": "af9e146f59b44a88fc6be2488b98ae78",
+    "url": "/static/media/sunny-bg.af9e146f.jpg"
+  },
+  {
+    "revision": "d82f3744c1da47c3c2cd95e2d5ed23dc",
+    "url": "/static/media/thunderstorm-bg.d82f3744.jpg"
+  }
+]);
